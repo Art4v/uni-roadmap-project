f2659fe35467e4ca59435daed64a041b0b6eb40e
Art4v <aarav_20@outlook.com>
2026-05-02 22:00:22 +1000

    data now saved to csv

--- data-scraping/learning/scraper.py
+++ data-scraping/learning/scraper.py
@@ -1,5 +1,6 @@
 # imports
 from bs4 import BeautifulSoup
+import csv
 import requests
 
 # parsing
@@ -10,6 +11,12 @@ soup = BeautifulSoup(page_to_scrape.text, "html.parser") # parse the website as 
 quotes = soup.find_all("span", attrs={"class": "text"}) # get all the quotes and save as a list
 authors = soup.find_all("small", attrs={"class": "author"}) # get all the authors and save as a list
 
-# print out quotes and authors
-for quote, author in zip(quotes, authors):
-    print(quote.text + ' - ' +  author.text)
+# save to CSV file
+with open("data.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Quote", "Author"])  # write header
+    
+    for quote, author in zip(quotes, authors):
+        writer.writerow([quote.text, author.text])
+
+print("Data saved to data.csv")
